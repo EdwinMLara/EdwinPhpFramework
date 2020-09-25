90191588588ab00666d5249081e63b4915ec7661
EdwinMLara <emlara35@gmail.com>
2020-09-24 19:39:43 -0500

    se unieron todas las librerias del EdwinFramework php pero aun faltan algunas

---
+++ Config/Consulta.php
@@ -0,0 +1,71 @@
+<?php
+class Consulta extends ConexionBaseDatos{
+    function __construct(){
+        parent::__construct();
+    }
+
+    public function tableExists($table){
+        $table_exists = $this->con->query('SHOW TABLES FROM '.DB_NAME.' LIKE "'.$this->con->deleteEspecialCharacters($table).'"');
+        if($table_exists){
+            if($this->con->num_rows($table_exists > 0)){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+    public function findByQuery($query){
+        $results = $this->con->query($query);
+        return $this->loopQuery($results);
+    }
+
+    public function insertByTable($table,$obj){
+        if($this->tableExists($table,$obj)){
+            $query = "INSERT INTO ".$this->deleteEspecialCharacters($table);
+            $query_properties = "(";
+            $query_values = "(";
+            foreach($obj as $property => $value){
+                if(strcmp($value,'autoincrement') == 0) continue;
+                $query_properties .=$property.",";
+                $query_values .= "'".$value."',";
+            }
+            $query_properties = substr($query_properties, 0, -1);
+            $query_values = substr($query_values, 0, -1);
+            $query = $query.$query_properties.") VALUES ".$query_values.")";
+            $this->con->query($query);
+            
+            return ($this->con->affected_rows() === 1) ? true : false;
+        }
+    }
+
+    public function findAllByTable($table){
+        if($this->tableExists($table)){
+            $query =  "SELECT * FROM ".$this->deleteEspecialCharacters($table);
+            return $this->findByquery($query);
+        }
+    }
+
+    public function updateByTable($table,$obj){
+        $query = "UPDATE ".$this->deleteEspecialCharacters($table)." SET ";
+        foreach($obj as $property => $value){
+            $query .= $property." = ".$value." ,";
+        }
+        $query = substr($query, 0, -1);
+        $this->con->query($query);
+        
+        return ($this->con->affected_rows() === 1) ? true : false;
+    }
+
+    public function deleteByTable($table,$id){
+        if($this->tableExists($table)){
+            $query = "DELETE FROM ".$this->deleteEspecialCharacters($table);
+            $query .= " WHERE id=". $this->deleteEspecialCharacters($id);
+            $query .= " LIMIT 1";
+            $this->con->query($query);
+            
+            return ($this->con->affected_rows() === 1) ? true : false;
+        }
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insoel</title>
+</head>
+<body>
+    BackEnd insoel APis
+</body>
+</html>
