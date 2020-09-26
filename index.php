2ec5bb4d9f157fdd0a4f6ebac9c6d355a502631c
EdwinMLara <emlara35@gmail.com>
2020-09-26 00:17:28 -0500

    ejecutando la api de usuarios

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insoel</title>
-</head>
-<body>
-    BackEnd insoel APis
-</body>
-</html>
+<?php
+    require_once("Config/funciones.php");
+    $api = new InsoelUsuariosApi();
+    $api->processApi();
+?>
