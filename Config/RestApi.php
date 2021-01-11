9f822e0596f6bc6b0539838018324e7829c7a597
EdwinMLara <emlara35@gmail.com>
2021-01-10 22:04:21 -0600

    Se obtiene el usuario por medio de la base de datos

--- Config/RestApi.php
+++ Config/RestApi.php
@@ -1,26 +1,31 @@
 <?php
+    require_once("JWT.php");
     require_once("Rest.php");
     class RestApi extends Rest{
-        public function __construct(){
-            parent::__construct();  
+        public $service;
+        
+        public function __construct($typeService){
+            parent::__construct(); 
+            $this->service = new Service($typeService); 
         }
 
-        public function generateToken($user){
+        /**El metodo generar token se utliza para generar un token valido
+         * el cual permitira realizar transacciones con la base de datos durante cierto periodo
+         * en este caso se utilizan 20 min
+        */
+
+        public function generateToken(){
+            $username = $this->validateParameter('username',$this->param["username"],STRING);
+            $password = $this->validateParameter('password',$this->param["password"],STRING);
+                  
+            $usuario = $this->service->getByField("usuario",$username);
+
             try{
-                
-                if(!is_object($user)){
-                    $this->returnResponse(INVALID_USER_PASS,"email or password is incorrect. ");
-                }
-
-                if($user->is_active == 0){
-                    $this->returnResponse(USER_NOT_ACTIVE,"The user not active.");
-                }
-
                 $payload = [
                     'iat' => time(),
                     'iss' => 'localhost',
-                    'exp' => time()+(5*60), //son segundo para que no se olvide
-                    'userId' => $user->id
+                    'exp' => time()+(20*60), //son segundo para que no se olvide
+                    'userId' => $usuario->idUsuario
                 ];
 
                 $token = JWT::encode($payload,SECRET_KEY);
