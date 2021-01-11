2a11e5172964648024346e490fc7d68882e1b745
EdwinMLara <emlara35@gmail.com>
2021-01-10 21:59:38 -0600

    Cambio de Nombre, se agregaron atributos y obtencion de argumentos correcta

--- Modelos/Usuarios.php
+++ Modelos/Usuarios.php
@@ -1,12 +1,14 @@
 <?php
-    class Usuario{
-        public static $tablename = "usuario";
+    class Usuarios{
+        public static $tablename = "usuarios";
         public String $idUsuario = "autoincrement";
         public String $username;
         public String $password;
+        public String $typeCount;
+        public String $time;
 
         public function __construct(){
-            $listArgs = func_get_args();
+            $listArgs = func_get_args()[0];
             $numArgs = count($listArgs);
 
             switch($numArgs){
@@ -16,9 +18,11 @@
                 case 2:
                     $this->__construct2($listArgs[0],$listArgs[1]);
                     break;
-                case 3:
-                    $this->__construct3($listArgs[0],$listArgs[1],$listArgs[2]);
+                case 5:
+                    $this->__construct3($listArgs[0],$listArgs[1],$listArgs[2],$listArgs[3],$listArgs[4]);
                     break;
+                default:
+                    echo $numArgs." No hay constructor de este tipo";
             }
         }
 
@@ -28,10 +32,12 @@
             $this->username = $username;
             $this->password = $password;
         }
-        public function __construct3($idUsuario,$username,$password){
+        public function __construct3($idUsuario,$username,$password,$typeCount,$time){
             $this->idUsuario = $idUsuario;
             $this->username = $username;
             $this->password = $password;
+            $this->typeCount = $typeCount;
+            $this->time = $time;
         }
 
     }
