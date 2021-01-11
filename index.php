129186012eac3f81d2bc5b178e1eecc481c068c2
EdwinMLara <emlara35@gmail.com>
2021-01-10 21:59:56 -0600

    Se elemino la basura

--- index.php
+++ index.php
@@ -2,18 +2,5 @@
     require_once("Api/InsoelUsuariosApi.php");
     
     $api = new InsoelUsuariosApi();
-    $api->processApi("InsoelUsuariosApi");
-    //echo "Tprobando";
+    $api->processApi();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
