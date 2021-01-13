b743f436718f563b7ed328197102d3db606e9935
EdwinMLara <emlara35@gmail.com>
2021-01-13 00:02:27 -0600

    Se agregaron campos al modelo

--- Modelos/Usuarios.php
+++ Modelos/Usuarios.php
@@ -5,7 +5,7 @@
         public String $username;
         public String $password;
         public String $typeCount;
-        public String $time;
+        public String $time = "now()";
 
         public function __construct(){
             $listArgs = func_get_args()[0];
@@ -15,8 +15,8 @@
                 case 0:
                     $this->__construct1();
                     break;
-                case 2:
-                    $this->__construct2($listArgs[0],$listArgs[1]);
+                case 3:
+                    $this->__construct2($listArgs[0],$listArgs[1],$listArgs[2]);
                     break;
                 case 5:
                     $this->__construct3($listArgs[0],$listArgs[1],$listArgs[2],$listArgs[3],$listArgs[4]);
@@ -28,9 +28,10 @@
 
         public function __construct1(){}
         
-        public function __construct2($username,$password){
+        public function __construct2($username,$password,$typeCount){
             $this->username = $username;
             $this->password = $password;
+            $this->typeCount = $typeCount;
         }
         public function __construct3($idUsuario,$username,$password,$typeCount,$time){
             $this->idUsuario = $idUsuario;
