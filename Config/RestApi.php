473a82ee44a655d836c45bcc8b98453725403a91
EdwinMLara <emlara35@gmail.com>
2021-01-13 21:45:26 -0600

    se ajusto al modelo de la tabla

--- Config/RestApi.php
+++ Config/RestApi.php
@@ -4,6 +4,10 @@
     class RestApi extends Rest{
         public $service;
         
+        /**El constructor recibe como parámetro el nombre la tabla de la cual se va a crear 
+         * el servicio, el cual proveerá los métodos crear, leer, actualizar, eliminar y buscar
+         * por tipo de columna
+         */
         public function __construct($typeService){
             parent::__construct(); 
             $this->service = new Service($typeService); 
@@ -17,14 +21,18 @@
         public function generateToken(){
             $username = $this->validateParameter('username',$this->param["username"],STRING);
             $password = $this->validateParameter('password',$this->param["password"],STRING);
-                  
+            
+            /**Se obtiene el usuario que coincida con el nombre de usuario, 
+             * el primer parametro es el nombre del campo en la tabla el cual debe ser exactamente igual al de la tabla
+             * el segundo argumento es el nombre a buscar dentro de la tabla 
+             */
             $usuario = $this->service->getByField("username",$username);
             try{
                 $payload = [
                     'iat' => time(),
                     'iss' => 'localhost',
                     'exp' => time()+(20*60), //son segundo para que no se olvide
-                    'userId' => $usuario->idUsuario
+                    'userId' => $usuario->id_usuario
                 ];
 
                 $token = JWT::encode($payload,SECRET_KEY);
